3213861e48098d73d18ed8b179a64d36f65867db
Younghoo Kim <sensitive.hu@gmail.com>
2024-07-25 23:45:20 +0900

    fix: fix broken style of HotPlaceListItem

--- src/components/HotplaceListItem.tsx
+++ src/components/HotplaceListItem.tsx
@@ -20,12 +20,15 @@ const HotPlaceListItem = ({
 }: HotPlaceItemProps) => (
   <Link href={`/contents/${idx}`}>
     <div className="flex flex-row">
-      <div className="w-[48px] h-[48px] relative shrink-0">
+      <div className="w-[48px] h-[48px] relative shrink-0 overflow-hidden">
         <CustomImage
           src={process.env.NEXT_PUBLIC_IMAGE_SERVER + thumbnail}
           alt={`${title} thumbnail`}
           width={48}
           height={48}
+          style={{
+            objectFit: "cover",
+          }}
         />
       </div>
       <div className="flex flex-col justify-around ml-[12px]">
@@ -33,7 +36,7 @@ const HotPlaceListItem = ({
           {title}
         </div>
         <div className="text-body5 text-grey-04">
-          {dayjs(startDate).format("YYYY-MM-DD")} -{" "}
+          {dayjs(startDate).format("YYYY.MM.DD")} -{" "}
           {dayjs(endDate).format("MM.DD")}
         </div>
       </div>
