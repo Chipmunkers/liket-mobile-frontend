44b7a93126e7487f914e609f9afb3d0bda707815
jochongs <joch2712@naver.com>
2024-08-08 12:54:00 +0900

    feat: Implement default marker display on map

--- src/components/KakaoMapV2/index.tsx
+++ src/components/KakaoMapV2/index.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import Script from "next/script";
 import { ReactNode, useEffect, useState } from "react";
-import { Map } from "react-kakao-maps-sdk";
+import { CustomOverlayMap, Map } from "react-kakao-maps-sdk";
 import axiosInstance from "../../utils/axios";
 import { ClusteredContent } from "./interface/ClusteredContent";
 import { getMapInfo } from "./util/getMapInfo";
+import { Content } from "./interface/Content";
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_MAP_API_KEY}&autoload=false`;
 
@@ -25,6 +26,7 @@ const KakaoMapV2 = ({ children }: { children?: ReactNode }) => {
     },
     level: 4,
   });
+  const [level, setLevel] = useState(4);
 
   const [clusteredContentList, setClusteredContentList] = useState<
     ClusteredContent[]
@@ -48,6 +50,53 @@ const KakaoMapV2 = ({ children }: { children?: ReactNode }) => {
       return data;
     },
   });
+
+  // * 현재 맵에 표시되고 있는 컨텐츠 목록
+  const [contentList, setContentList] = useState<Content[]>([]);
+
+  const { data: contentApiResult } = useQuery<{ contentList: Content[] }>({
+    queryKey: ["map-contents", mapInfo],
+    queryFn: async () => {
+      if (mapInfo.level > 5) return;
+
+      const { data } = await axiosInstance.get(
+        `/apis/map/culture-content/all?` +
+          `top-x=${mapInfo.bound.top.x}&` +
+          `top-y=${mapInfo.bound.top.y}&` +
+          `bottom-x=${mapInfo.bound.bottom.x}&` +
+          `bottom-y=${mapInfo.bound.bottom.y}&`
+      );
+
+      return data;
+    },
+  });
+
+  useEffect(() => {
+    if (!contentApiResult) return;
+
+    const alreadyExistContentIdxList = contentList.map(
+      (content) => content.idx
+    );
+
+    setContentList([
+      // * 이미 존재하는 컨텐츠 데이터 삭제하기
+      ...contentApiResult.contentList.filter(
+        (content) => !alreadyExistContentIdxList.includes(content.idx)
+      ),
+      // * 현재 보고있는 bound를 벗어난 컨텐츠 삭제하기
+      ...contentList.filter(
+        (content) =>
+          content.location.positionX <= mapInfo.bound.bottom.x &&
+          content.location.positionX >= mapInfo.bound.top.x &&
+          content.location.positionY <= mapInfo.bound.top.y &&
+          content.location.positionY >= mapInfo.bound.bottom.y
+      ),
+    ]);
+  }, [contentApiResult]);
+
+  useEffect(() => {
+    setClusteredContentList([]);
+  }, [level]);
 
   useEffect(() => {
     if (!clusteredApiResult) return;
@@ -89,8 +138,59 @@ const KakaoMapV2 = ({ children }: { children?: ReactNode }) => {
         className="grow relative w-[100%]"
         level={4}
         onDragEnd={(map) => setMapInfo(getMapInfo(map))}
-        onZoomChanged={(map) => setMapInfo(getMapInfo(map))}
-      ></Map>
+        onZoomChanged={(map) => {
+          setLevel(map.getLevel());
+          setMapInfo(getMapInfo(map));
+        }}
+      >
+        {clusteredContentList.map((clusteredContent) => (
+          <CustomOverlayMap
+            position={{
+              lat: clusteredContent.lat,
+              lng: clusteredContent.lng,
+            }}
+          >
+            <div>{clusteredContent.count}</div>
+          </CustomOverlayMap>
+        ))}
+        {contentList.map((content) => (
+          <CustomOverlayMap
+            position={{
+              lng: content.location.positionX,
+              lat: content.location.positionY,
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                width={"24px"}
+                src={`https://liket.s3.ap-northeast-2.amazonaws.com/map-marker/default-marker-${content.genre.idx}.svg`}
+              />
+              <div
+                style={{
+                  width: "80px",
+                  textAlign: "center",
+                  wordWrap: "break-word",
+                  whiteSpace: "normal",
+                  marginTop: "4px",
+                  fontSize: "12px",
+                  lineHeight: "14.4px",
+                  color: "#222",
+                  textShadow:
+                    "-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff",
+                }}
+              >
+                {content.title}
+              </div>
+            </div>
+          </CustomOverlayMap>
+        ))}
+      </Map>
     </>
   );
 };
