b9c0a162d69c4d7b8d4ef9b544e18ff981a925e2
jochongs <joch2712@naver.com>
2024-08-12 16:36:09 +0900

    feat: Add kakao map in detial content

--- src/app/contents/[idx]/components/ContentDetailInfo.tsx
+++ src/app/contents/[idx]/components/ContentDetailInfo.tsx
@@ -2,6 +2,10 @@ import Divider from "@/components/Divider";
 import { colors } from "@/utils/style";
 import RightArrowIcon from "@/icons/right-arrow.svg";
 import { ContentEntity } from "@/types/api/culture-content";
+import Script from "next/script";
+import { CustomOverlayMap, Map } from "react-kakao-maps-sdk";
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_MAP_API_KEY}&autoload=false`;
 
 const ContentDetailInfo = (props: { content: ContentEntity }) => {
   const { content } = props;
@@ -19,6 +23,45 @@ const ContentDetailInfo = (props: { content: ContentEntity }) => {
           {content.location.address} {content.location.detailAddress}
         </div>
         <div className="h-[171px] w-[100%] bg-grey-02 flex">
+          <Script
+            strategy="beforeInteractive"
+            type="text/javascript"
+            src={KAKAO_SDK_URL}
+          />
+          <Map
+            className="w-[100%] h-[100%]"
+            center={{
+              lng: content.location.positionX,
+              lat: content.location.positionY,
+            }}
+            isPanto={false}
+            level={4}
+          >
+            <CustomOverlayMap
+              position={{
+                lng: content.location.positionX,
+                lat: content.location.positionY,
+              }}
+              key={content.idx}
+            >
+              <div className="flex items-end justify-center h-[50px] select-none cursor-pointer">
+                <img
+                  className="w-[30px]"
+                  src={`https://liket.s3.ap-northeast-2.amazonaws.com/map-marker/click_marker_${content.genre.idx}_icon.svg`}
+                />
+              </div>
+              <div
+                className="w-[120px] mt-[4px] text-base text-center text-wrap whitespace-nowrap leading-[14.4px] text-white p-[8px] rounded-[16px]"
+                style={{
+                  textShadow:
+                    "-1px -1px 0 #222, 1px -1px 0 #222, -1px 1px 0 #222, 1px 1px 0 #222",
+                  backgroundColor: "rgba(0, 0, 0, 0.6)",
+                }}
+              >
+                {content.title}
+              </div>
+            </CustomOverlayMap>
+          </Map>
           {/* TODO: 맵 추가 */}
         </div>
         <button className="center absolute right-[24px] bottom-0 text-button4 text-skyblue-03">
