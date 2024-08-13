254c7597cfe1999a8bd9684ccab4a616e464140a
jochongs <joch2712@naver.com>
2024-08-13 15:45:41 +0900

    fix: Fix signup 정규표현식

--- src/components/SignupForm/ProfileForm.tsx
+++ src/components/SignupForm/ProfileForm.tsx
@@ -19,8 +19,8 @@ const profileScheme = z
       .string()
       .min(2, { message: "2~15자 이내로 입력해주세요." })
       .max(15, { message: "2~15자 이내로 입력해주세요." })
-      .regex(/^[a-zA-Z0-9]+$/, {
-        message: "닉네임은 영문, 숫자만 입력할 수 있습니다.",
+      .regex(/^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$/, {
+        message: "닉네임 2-8글자의 영문, 숫자만 입력할 수 있습니다.",
       }),
     gender: z.string().min(1),
     file: z.string().min(1),
