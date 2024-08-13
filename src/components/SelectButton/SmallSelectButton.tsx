453954191bffd6c53101879f05e530e4006a5c2c
jochongs <joch2712@naver.com>
2024-08-14 00:03:38 +0900

    feat: Add 좋아요 페이지 top layout

--- src/components/SelectButton/SmallSelectButton.tsx
+++ src/components/SelectButton/SmallSelectButton.tsx
@@ -8,6 +8,7 @@ interface Props {
   text: string;
   placeholder: string;
   onClick: EmptyFunction;
+  className?: string;
 }
 
 const SmallSelectButton = ({
@@ -16,6 +17,7 @@ const SmallSelectButton = ({
   text,
   placeholder,
   onClick,
+  className,
 }: Props) => {
   return (
     <button
@@ -24,7 +26,8 @@ const SmallSelectButton = ({
         "flex justify-between items-center rounded-[20px] text-button3",
         withBorder && "border-solid border-[1px] border-grey-02",
         text && "bg-skyblue-01 text-white",
-        "h-[28px] pl-[8px] pr-[6px]"
+        "h-[28px] pl-[8px] pr-[6px]",
+        className || ""
       )}
     >
       {text || placeholder}
