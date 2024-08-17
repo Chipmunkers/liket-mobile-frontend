abaf55f2429d18b7ae28d3261ae9169d07594a05
jochongs <joch2712@naver.com>
2024-08-17 15:48:32 +0900

    chore: Add code comment

--- src/hooks/useIsWebView.ts
+++ src/hooks/useIsWebView.ts
@@ -1,6 +1,7 @@
 export const WEB_VIEW_FLAG = "WEB_VIEW";
 
 export const useIsWebView = (userAgent?: string) => {
+  // ! navigate 코드 빌드때문에 삭제했음
   // 클라이언트 사이드
   if (!userAgent) {
     userAgent = "";
