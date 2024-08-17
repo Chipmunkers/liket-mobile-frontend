a8eb1c7721131901d5c481b03f583654e03d9827
jochongs <joch2712@naver.com>
2024-08-17 15:41:26 +0900

    fix: Fix navigator bug

--- src/hooks/useIsWebView.ts
+++ src/hooks/useIsWebView.ts
@@ -1,11 +1,9 @@
-import { headers } from "next/headers";
-
 export const WEB_VIEW_FLAG = "WEB_VIEW";
 
 export const useIsWebView = (userAgent?: string) => {
   // 클라이언트 사이드
   if (!userAgent) {
-    userAgent = navigator.userAgent;
+    userAgent = navigator?.userAgent || "";
   }
 
   return userAgent.endsWith(WEB_VIEW_FLAG);
