ad6a6d458143cf5b1ac262c627db8f63cb74841c
jochongs <joch2712@naver.com>
2024-08-17 15:45:15 +0900

    fix: Delete navigator

--- src/hooks/useIsWebView.ts
+++ src/hooks/useIsWebView.ts
@@ -3,7 +3,7 @@ export const WEB_VIEW_FLAG = "WEB_VIEW";
 export const useIsWebView = (userAgent?: string) => {
   // 클라이언트 사이드
   if (!userAgent) {
-    userAgent = navigator?.userAgent || "";
+    userAgent = "";
   }
 
   return userAgent.endsWith(WEB_VIEW_FLAG);
