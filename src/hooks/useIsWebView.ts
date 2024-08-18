6f8d1eaaa77e55156d6c77d376f5b4c0dcbc70af
jochongs <joch2712@naver.com>
2024-08-18 19:36:11 +0900

    chore: Update useIsWebView

--- src/hooks/useIsWebView.ts
+++ src/hooks/useIsWebView.ts
@@ -1,7 +1,7 @@
 export const WEB_VIEW_FLAG = "WEB_VIEW";
 
 export const useIsWebView = () => {
-  if (typeof window === "undefined") return false;
+  if (typeof window === "undefined") return true;
 
   if (window.isWebview === undefined) return false;
 
