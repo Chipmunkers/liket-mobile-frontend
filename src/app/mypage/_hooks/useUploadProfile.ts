b4f616101e69d9f81789ee8f18afd84b57799810
jochongs <joch2712@naver.com>
2024-08-19 19:37:28 +0900

    feat: Implement update profile image in mypage

---
+++ src/app/mypage/_hooks/useUploadProfile.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "@/utils/axios";
+import { UploadedFileEntity } from "@/types/api/upload";
+
+const useUploadProfile = () =>
+  useMutation({
+    mutationFn: async (formData: FormData) => {
+      const { data } = await axiosInstance.post<UploadedFileEntity>(
+        "/apis/upload/profile-img",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      return data;
+    },
+  });
+
+export default useUploadProfile;
