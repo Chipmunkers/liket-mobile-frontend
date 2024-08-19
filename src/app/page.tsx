c7e12b4faca889a49c01db48ad3ad5f6c526aa9d
jochongs <joch2712@naver.com>
2024-08-19 15:22:33 +0900

    chore: Delete unncessary console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,10 +65,9 @@ const Home = async () => {
             <section className="mb-[24px] text-h2">
               <h2 className="pl-[24px] mb-[8px]">최근 인기 리뷰</h2>
               <CustomScrollContainer className="flex flex-row gap-[8px] overflow-x-hidden overflow-y-hidden w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
-                {reviews.map((review, index) => {
-                  console.log(reviews);
-                  return <ReviewCard key={index} review={review} />;
-                })}
+                {reviews.map((review, index) => (
+                  <ReviewCard key={index} review={review} />
+                ))}
               </CustomScrollContainer>
             </section>
           </>
