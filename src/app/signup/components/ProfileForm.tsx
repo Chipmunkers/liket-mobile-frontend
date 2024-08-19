89c7b7093501e8996562ac82b541ac7f2b6b39de
jochongs <joch2712@naver.com>
2024-08-19 18:49:34 +0900

    feat: Change nickname reg exp

--- src/app/signup/components/ProfileForm.tsx
+++ src/app/signup/components/ProfileForm.tsx
@@ -20,8 +20,9 @@ const profileScheme = z
       .string()
       .min(2, { message: "2~15자 이내로 입력해주세요." })
       .max(15, { message: "2~15자 이내로 입력해주세요." })
-      .regex(/^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$/, {
-        message: "닉네임 2-8글자의 영문, 숫자만 입력할 수 있습니다.",
+      .regex(/^[a-zA-Z0-9가-힣_-]{2,8}$/g, {
+        message:
+          "닉네임 2-8글자의 한글, 영문, 숫자, _, - 만 입력할 수 있습니다.",
       }),
     gender: z.string().min(1),
     file: z.string().min(1),
