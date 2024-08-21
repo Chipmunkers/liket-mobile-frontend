8d0f1b96256154f5b08321b873795977f7199d60
jochongs <joch2712@naver.com>
2024-08-21 13:32:02 +0900

    refactor: Refactor HotStyleSection component to not use legacy style const data

--- src/app/_ui/HotStyleSection.tsx
+++ src/app/_ui/HotStyleSection.tsx
@@ -1,13 +1,13 @@
 "use client";
 
+import { STYLES } from "@/shared/consts/content/style";
 import { useGetHotStyleContents } from "../_hooks/getHotContents";
-import { styles } from "../../../public/data/style";
 import ContentCardSection from "@/widgets/content/ContentSection";
 
 const HotStyleSection = () => {
   const { data } = useGetHotStyleContents();
   const style =
-    styles.find((style) => style.idx === data?.style.idx) || styles[0];
+    STYLES.find((style) => style.idx === data?.style.idx) || STYLES[0];
 
   return (
     <ContentCardSection contentList={data?.contentList}>
