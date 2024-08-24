eaac9ab6e8333db9235474a628608e71e1496b33
jochongs <joch2712@naver.com>
2024-08-24 15:54:59 +0900

    feat: 메인 페이지 컨텐츠 셔플해서 보여주도록 변경

--- src/app/_ui/HotStyleSection.tsx
+++ src/app/_ui/HotStyleSection.tsx
@@ -3,6 +3,7 @@
 import { STYLES } from "@/shared/consts/content/style";
 import { useGetHotStyleContents } from "../_hooks/getHotContents";
 import ContentCardSection from "@/widgets/content/ContentSection";
+import { shuffle } from "@/shared/helpers/shuffle";
 
 const HotStyleSection = () => {
   const { data } = useGetHotStyleContents();
@@ -10,7 +11,9 @@ const HotStyleSection = () => {
     STYLES.find((style) => style.idx === data?.style.idx) || STYLES[0];
 
   return (
-    <ContentCardSection contentList={data?.contentList}>
+    <ContentCardSection
+      contentList={data?.contentList ? shuffle(data?.contentList) : undefined}
+    >
       {style.title.split("{style_name}")[0]}
       <span className="text-skyblue-01">#{data?.style.name}</span>
       {style.title.split("{style_name}")[1]}
