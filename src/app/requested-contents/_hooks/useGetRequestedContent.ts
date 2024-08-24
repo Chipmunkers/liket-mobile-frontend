ff558abec76ae36b8712949608f8f7867b2c4bd6
jochongs <joch2712@naver.com>
2024-08-24 13:03:28 +0900

    feat: Implement requested content page

--- src/app/requested-contents/_hooks/useGetRequestedContent.ts
+++ src/app/requested-contents/_hooks/useGetRequestedContent.ts
@@ -1,13 +1,29 @@
+import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
 import axiosInstance from "@/shared/helpers/axios";
-import { ContentEntity } from "@/shared/types/api/content/ContentEntity";
+import customToast from "@/shared/helpers/customToast";
+import { stackRouterPush } from "@/shared/helpers/stackRouter";
+import { SummaryContentEntity } from "@/shared/types/api/content/SummaryContentEntity";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const useGetRequestedContent = (idx: number | undefined) => {
+  const router = useRouter();
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
+
   const res = useInfiniteQuery({
     queryKey: ["requested-contents", idx],
     queryFn: async ({ pageParam = 1 }) => {
+      if (!idx)
+        return {
+          contentList: [],
+          nextPage: undefined, // 다음 페이지 번호 계산
+          isLastPage: 0,
+        };
+
       const { data } = await axiosInstance.get<{
-        contentList: ContentEntity[];
+        contentList: SummaryContentEntity[];
       }>(`/apis/culture-content/all?user=${idx}&page=${pageParam}`);
 
       return {
@@ -24,8 +40,45 @@ export const useGetRequestedContent = (idx: number | undefined) => {
     retry: 0,
   });
 
+  useEffect(() => {
+    const error = res.error as AxiosError;
+
+    if (!error) return;
+
+    if (error.response?.status === 401) {
+      stackRouterPush(router, {
+        path: "/login?isTokenExpired=true",
+        screen: WEBVIEW_SCREEN.LOGIN,
+        isStack: false,
+      });
+      return;
+    }
+
+    console.log(error);
+
+    customToast("예상하지 못한 에러가 발생했습니다. 다시 시도해주세요.");
+  }, [res.error]);
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !res.isFetching && !res.error) {
+          res.fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    observer.observe(target);
+    return () => {
+      observer.unobserve(target);
+    };
+  }, [target, res.hasNextPage, res.isFetching]);
+
   return {
     ...res,
-    contentListToShow: res.data?.pages.flat(),
+    setTarget,
   };
 };
--- src/app/requested-contents/page.tsx
+++ src/app/requested-contents/page.tsx
@@ -13,38 +13,26 @@ import { useGetMyInfo } from "@/shared/hooks/useGetMyInfo";
 import ContentCardSmall from "@/entities/content/ContentCardSmall";
 import { DefaultLoading } from "@/shared/ui/Loading";
 import ReloadButton from "@/shared/ui/ReloadButton";
+import { stackRouterPush } from "@/shared/helpers/stackRouter";
+import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
 
 export default function Page() {
   const router = useRouter();
   const { data: myInformationData, error: myInformationError } = useGetMyInfo();
-  const { data, fetchNextPage, isFetching, refetch, error, hasNextPage } =
-    useGetRequestedContent(myInformationData?.idx);
 
-  const [target, setTarget] = useState<HTMLDivElement | null>(null);
+  const { data, refetch, error, setTarget } = useGetRequestedContent(
+    myInformationData?.idx
+  );
 
   useEffect(() => {
     if (myInformationError?.response?.status === 401) {
-      router.replace("/login");
+      stackRouterPush(router, {
+        path: "/login?isTokenExpired=true",
+        screen: WEBVIEW_SCREEN.LOGIN,
+        isStack: false,
+      });
     }
   }, [myInformationError?.response?.status, router]);
-
-  useEffect(() => {
-    if (!target) return;
-
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting && !isFetching && !error) {
-          fetchNextPage();
-        }
-      },
-      { threshold: 1 }
-    );
-
-    observer.observe(target);
-    return () => {
-      observer.unobserve(target);
-    };
-  }, [target, hasNextPage, isFetching]);
 
   return (
     <>
@@ -57,8 +45,7 @@ export default function Page() {
       </Header>
       <main className="px-[24px]">
         {!data && <DefaultLoading />}
-        {!data ? <DefaultLoading center /> : null}
-        {data?.pages[0].contentList.length === 0 ? (
+        {data?.pages[0].contentList.length === 0 && myInformationData ? (
           <div className="empty">컨텐츠 등록 요청 내역이 없습니다.</div>
         ) : (
           <ul>
