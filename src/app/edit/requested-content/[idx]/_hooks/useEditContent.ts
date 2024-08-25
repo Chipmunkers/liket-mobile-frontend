097ae942a348bc1245fd2a28d254e9f6a6d0cc3e
jochongs <joch2712@naver.com>
2024-08-25 20:49:17 +0900

    fix: Fix 409 에러 예외 메세지

--- src/app/edit/requested-content/[idx]/_hooks/useEditContent.ts
+++ src/app/edit/requested-content/[idx]/_hooks/useEditContent.ts
@@ -58,7 +58,7 @@ export const useEditContent = ({
 
       if (statusCode === 409) {
         return customToast(
-          "활성화된 컨텐츠는 수정할 수 없습니다.\n문의주시기 바랍니다."
+          "활성화된 컨텐츠는 수정할 수 없습니다. 문의를 남겨주세요."
         );
       }
 
