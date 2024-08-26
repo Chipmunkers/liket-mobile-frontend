876913b7264d09db9436714b5214ed427a5f1912
jochongs <joch2712@naver.com>
2024-08-26 21:29:52 +0900

    feat: Implement inquiry not found error handling

--- src/app/inquiries/[idx]/page.tsx
+++ src/app/inquiries/[idx]/page.tsx
@@ -14,6 +14,7 @@ import CustomScrollContainer from "@/shared/ui/CustomScrollContainer";
 import DefaultImg from "@/shared/ui/DefaultImg";
 import Divider from "@/shared/ui/Divider";
 import { classNames } from "@/shared/helpers/classNames";
+import { useEffect } from "react";
 
 interface PageProps {
   params: {
@@ -22,9 +23,13 @@ interface PageProps {
 }
 
 export default function page({ params: { idx } }: PageProps) {
-  const { data: inquiry } = useGetInquiryByIdx(Number(idx));
+  const { data: inquiry, error } = useGetInquiryByIdx(Number(idx));
 
-  if (!inquiry) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  if (error?.response?.status === 404) {
     return (
       <>
         <Header>
@@ -35,8 +40,69 @@ export default function page({ params: { idx } }: PageProps) {
           />
           <HeaderMiddle text="문의내역" />
         </Header>
-        <main className="boxShadow h-fit">
-          <InquirySkeleton />
+        <main className="flex items-center justify-center">
+          <div className="flex flex-col items-center">
+            <span className="text-h1">문의내역을 찾을 수 없습니다.</span>
+            <span className="text-body3 text-grey-04 mt-[16px]">
+              존재하지 않거나
+            </span>
+            <span className="text-body3 text-grey-04">
+              삭제된 문의내역입니다.
+            </span>
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  if (inquiry) {
+    return (
+      <>
+        <Header>
+          <HeaderLeft
+            option={{
+              back: true,
+            }}
+          />
+          <HeaderMiddle text="문의내역" />
+        </Header>
+        <main>
+          <section className="px-[24px]">
+            <InquiryCardSmall
+              inquiry={{ ...inquiry, isAnswered: !!inquiry.answerList[0] }}
+            />
+            <div className="py-[16px] border-b-[1px] border-b-grey-01">
+              <div className="text-body3 h-[112px]">{inquiry.contents}</div>
+            </div>
+          </section>
+          <section className="mt-[16px]">
+            <CustomScrollContainer className="flex flex-row gap-[8px] w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
+              {inquiry.imgList.map((imgPath) => (
+                <div className="w-[80px] h-[80px] relative border-[1px] border-grey-02">
+                  <DefaultImg src={imgPath} alt="" />
+                </div>
+              ))}
+            </CustomScrollContainer>
+          </section>
+          <Divider width="100%" height="8px" margin="24px 0px" />
+          <section className="px-[24px]">
+            <div className="text-h2">답변</div>
+            <div
+              className={classNames(
+                "py-[16px] border-y-[1px] border-grey-01 mt-[8px] min-h-[132px]",
+
+                inquiry.answerList[0] ? "" : "flex items-center justify-center"
+              )}
+            >
+              {inquiry.answerList[0] ? (
+                inquiry.answerList[0].contents
+              ) : (
+                <span className="text-body4 text-grey-03">
+                  아직 답변중입니다!
+                </span>
+              )}
+            </div>
+          </section>
         </main>
       </>
     );
@@ -53,42 +119,7 @@ export default function page({ params: { idx } }: PageProps) {
         <HeaderMiddle text="문의내역" />
       </Header>
       <main>
-        <section className="px-[24px]">
-          <InquiryCardSmall
-            inquiry={{ ...inquiry, isAnswered: !!inquiry.answerList[0] }}
-          />
-          <div className="py-[16px] border-b-[1px] border-b-grey-01">
-            <div className="text-body3 h-[112px]">{inquiry.contents}</div>
-          </div>
-        </section>
-        <section className="mt-[16px]">
-          <CustomScrollContainer className="flex flex-row gap-[8px] w-[100%] [&>*:last-child]:mr-[24px] [&>*:first-child]:ml-[24px]">
-            {inquiry.imgList.map((imgPath) => (
-              <div className="w-[80px] h-[80px] relative border-[1px] border-grey-02">
-                <DefaultImg src={imgPath} alt="" />
-              </div>
-            ))}
-          </CustomScrollContainer>
-        </section>
-        <Divider width="100%" height="8px" margin="24px 0px" />
-        <section className="px-[24px]">
-          <div className="text-h2">답변</div>
-          <div
-            className={classNames(
-              "py-[16px] border-y-[1px] border-grey-01 mt-[8px] min-h-[132px]",
-
-              inquiry.answerList[0] ? "" : "flex items-center justify-center"
-            )}
-          >
-            {inquiry.answerList[0] ? (
-              inquiry.answerList[0].contents
-            ) : (
-              <span className="text-body4 text-grey-03">
-                아직 답변중입니다!
-              </span>
-            )}
-          </div>
-        </section>
+        <InquirySkeleton />
       </main>
     </>
   );
