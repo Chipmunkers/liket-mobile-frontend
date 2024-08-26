ed677b3d93e6606c211531e5f5abfe5f7b5e62e7
jochongs <joch2712@naver.com>
2024-08-26 21:54:30 +0900

    fix: Fix hot review profile img component

--- src/entities/review/ReviewCard/index.tsx
+++ src/entities/review/ReviewCard/index.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import DefaultImg from "@/shared/ui/DefaultImg";
 import { ReviewEntity } from "@/shared/types/api/review/ReviewEntity";
+import DefaultProfileIcon from "@/shared/icon/user/DefaultProfileIcon.svg";
+import { colors } from "@/shared/style/color";
 
 const ReviewCard = (props: { review: ReviewEntity }) => {
   const { review } = props;
@@ -39,8 +41,17 @@ const ReviewCard = (props: { review: ReviewEntity }) => {
           <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-grey-black to-transparent"></div>
         </div> */}
         <div className="absolute flex flex-row mt-[12px] ml-[12px] top-0">
-          <div className="relative w-[18px] h-[18px] overflow-hidden rounded-full border-[1px] border-grey-02">
-            <DefaultImg src={review.author.profileImgPath || ""} />
+          <div className="relative w-[18px] h-[18px] overflow-hidden rounded-full border-[1px] border-grey-02 bg-grey-01 flex justify-center items-center">
+            <DefaultImg
+              src={review.author.profileImgPath || ""}
+              className="flex justify-center items-center"
+              fallbackComponent={
+                <DefaultProfileIcon
+                  className="w-[12px] h-[12px]"
+                  fill={colors.grey.black}
+                />
+              }
+            />
           </div>
           <div className="text-body2 text-white ml-[4px]">
             {review.author.nickname}
@@ -51,7 +62,7 @@ const ReviewCard = (props: { review: ReviewEntity }) => {
             {review.description}
           </div>
           <div className="text-body5 text-grey-02 w-[118px] flex">
-            <div className="line-clamp-1">${review.cultureContent.title}</div>
+            <div className="line-clamp-1">{review.cultureContent.title}</div>
             <RightArrowIcon className="fill-grey-02" width="16px" />
           </div>
         </div>
