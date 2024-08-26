5725b1bfc8bcc329fa90ca820ce0ff02c11a2765
jochongs <joch2712@naver.com>
2024-08-26 17:06:11 +0900

    feat: Implement error exception handling in create inquiry hooks

--- src/app/create/inquiry/hooks/useCreateInquiry.ts
+++ src/app/create/inquiry/hooks/useCreateInquiry.ts
@@ -1,6 +1,10 @@
+import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
 import axiosInstance from "@/shared/helpers/axios";
+import customToast from "@/shared/helpers/customToast";
+import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
 
 interface CreateInquiryDto {
   title: string;
@@ -17,8 +21,25 @@ export const useCreateInquiry = (
     AxiosError,
     CreateInquiryDto
   >
-) =>
-  useMutation({
+) => {
+  const router = useRouter();
+
+  return useMutation({
     mutationFn: (param) => axiosInstance.post(`/apis/inquiry`, param),
+    onError: (err) => {
+      const statusCode = err.response?.status;
+
+      if (statusCode === 401) {
+        stackRouterPush(router, {
+          path: "/login?isTokenExpired=true",
+          screen: WEBVIEW_SCREEN.LOGIN,
+          isStack: false,
+        });
+        return;
+      }
+
+      customToast("예상하지 못한 에러가 발생했습니다. 다시 시도해주세요.");
+    },
     ...props,
   });
+};
