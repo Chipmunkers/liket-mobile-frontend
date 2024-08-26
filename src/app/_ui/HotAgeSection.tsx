b98c7c981d41b91d02999baaf3176034763f6106
jochongs <joch2712@naver.com>
2024-08-26 23:09:17 +0900

    fix: Fix hot age title layout

--- src/app/_ui/HotAgeSection.tsx
+++ src/app/_ui/HotAgeSection.tsx
@@ -12,8 +12,8 @@ const HotAgeSection = () => {
       contentList={data?.contentList ? shuffle(data?.contentList) : undefined}
     >
       요즘
-      <span className="text-skyblue-01">#{data?.age.name}</span>
-      연령대가 주목하는 곳 ✨
+      <span className="text-skyblue-01"> #{data?.age.name} </span>연령대가
+      주목하는 곳 ✨
     </ContentCardSection>
   );
 };
