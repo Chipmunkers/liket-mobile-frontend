540a19981a1f74e3ae8e5bf67ae5c2aca6fee253
jochongs <joch2712@naver.com>
2024-08-27 11:43:31 +0900

    feat: Implement inquiry group skeleton ui

--- src/app/inquiries/_ui/InquiryInfiniteGroup/index.tsx
+++ src/app/inquiries/_ui/InquiryInfiniteGroup/index.tsx
@@ -1,11 +1,13 @@
 import InquiryCardSmall from "@/entities/inquiry/InquiryCardSmall";
 import { useGetInquiries } from "./hooks/useGetInquiries";
+import InquiryGroupSkeleton from "./ui/InquiryGroupSkeleton";
 
 const InquiryInfiniteGroup = () => {
   const { data, setTarget } = useGetInquiries();
 
   return (
     <ul className="w-[100%] px-[24px] flex flex-col">
+      {!data && <InquiryGroupSkeleton />}
       {data &&
         data.pages
           .map((page) => page.inquiryList)
