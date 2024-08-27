c546400fd3786d998551704e3e8505913ddf2f3b
jochongs <joch2712@naver.com>
2024-08-27 11:49:43 +0900

    feat: Implement empty inquiry layout

--- src/app/inquiries/_ui/InquiryInfiniteGroup/index.tsx
+++ src/app/inquiries/_ui/InquiryInfiniteGroup/index.tsx
@@ -7,7 +7,17 @@ const InquiryInfiniteGroup = () => {
 
   return (
     <ul className="w-[100%] px-[24px] flex flex-col">
+      {/* 스켈레톤 UI */}
       {!data && <InquiryGroupSkeleton />}
+
+      {/* 문의 없을 때 */}
+      {!data?.pages[0].inquiryList[0] && (
+        <div className="absolute text-body3 text-grey-03 top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%]">
+          문의내역이 없습니다.
+        </div>
+      )}
+
+      {/* 문의 있을 때 */}
       {data &&
         data.pages
           .map((page) => page.inquiryList)
