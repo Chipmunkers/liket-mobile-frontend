c3890ea8446ca39c331648286b714ba4dc1509a7
jochongs <joch2712@naver.com>
2024-08-28 15:55:15 +0900

    feat: Implement review meatball button click event

--- src/app/reviews/_ui/ReviewInfinite/index.tsx
+++ src/app/reviews/_ui/ReviewInfinite/index.tsx
@@ -7,51 +7,92 @@ import { Props } from "./types";
 import ReloadButton from "@/shared/ui/ReloadButton";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { useRouter } from "next/navigation";
+import Drawer from "@/shared/ui/Drawer";
+import { useState } from "react";
+import { ButtonBase } from "@mui/material";
+import customToast from "@/shared/helpers/customToast";
 
 const ReviewInfinite = ({ idx }: Props) => {
   const router = useRouter();
   const { data, setTarget, error, refetch } = useGetMyReviews(idx);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedReview, setSelectedReview] = useState<number>();
 
   return (
-    <div>
-      {!data &&
-        !error &&
-        Array(10)
-          .fill(0)
-          .map(() => (
-            <>
-              <div className="px-[24px]">
-                <ReviewLargeSkeleton />
-              </div>
-              <Divider width="100%" height="8px" margin="8px 0" />
-            </>
-          ))}
-      {data &&
-        data.pages
-          .map((page) => page.reviewList)
-          .flat()
-          .map((review) => (
-            <>
-              <div className="px-[24px]">
-                <ReviewLarge
-                  review={review}
-                  onClickContents={(review) => {
-                    stackRouterPush(router, {
-                      path: `/contents/${review.cultureContent.idx}?review=${review.idx}&tab=review`,
-                    });
-                  }}
-                />
-              </div>
-              <Divider width="100%" height="8px" margin="8px 0" />
-            </>
-          ))}
-      {error && (
-        <ReloadButton className="pt-[16px] pb-[24px]" onClick={() => refetch()}>
-          새고로침
-        </ReloadButton>
-      )}
-      <div ref={setTarget}></div>
-    </div>
+    <>
+      <div>
+        {!data &&
+          !error &&
+          Array(10)
+            .fill(0)
+            .map(() => (
+              <>
+                <div className="px-[24px]">
+                  <ReviewLargeSkeleton />
+                </div>
+                <Divider width="100%" height="8px" margin="8px 0" />
+              </>
+            ))}
+        {data &&
+          data.pages
+            .map((page) => page.reviewList)
+            .flat()
+            .map((review) => (
+              <>
+                <div className="px-[24px]">
+                  <ReviewLarge
+                    review={review}
+                    onClickContents={(review) => {
+                      stackRouterPush(router, {
+                        path: `/contents/${review.cultureContent.idx}?review=${review.idx}&tab=review`,
+                      });
+                    }}
+                    onClickMeatball={(review) => {
+                      setSelectedReview(review.idx);
+                      setIsDrawerOpen(true);
+                    }}
+                  />
+                </div>
+                <Divider width="100%" height="8px" margin="8px 0" />
+              </>
+            ))}
+        {error && (
+          <ReloadButton
+            className="pt-[16px] pb-[24px]"
+            onClick={() => refetch()}
+          >
+            새고로침
+          </ReloadButton>
+        )}
+        <div ref={setTarget}></div>
+      </div>{" "}
+      <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <li className="bottom-sheet-list">
+          <ButtonBase
+            onClick={() => {
+              return customToast("열심히 준비중입니다!");
+              // TODO: 페이지 생성 후 붙여야함
+              router.push(`/edit/review/${selectedReview}`);
+            }}
+            className="bottom-sheet-button flex justify-start px-[24px]"
+          >
+            수정하기
+          </ButtonBase>
+        </li>
+        <li className="bottom-sheet-list">
+          <ButtonBase
+            onClick={() => {
+              if (!selectedReview) return;
+
+              selectedReview;
+            }}
+            className="bottom-sheet-button flex justify-start px-[24px] text-rosepink-01"
+          >
+            삭제
+          </ButtonBase>
+        </li>
+      </Drawer>
+    </>
   );
 };
 
