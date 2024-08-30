1ed9f17d455bb0b2a2b2f4a74ff1cd70e85d5d96
jochongs <joch2712@naver.com>
2024-08-30 14:18:50 +0900

    feat: Add exception handler to account page

--- src/app/account/_hooks/useLogout.ts
+++ src/app/account/_hooks/useLogout.ts
@@ -1,15 +1,20 @@
 import axiosInstance from "@/shared/helpers/axios";
-import customToast from "@/shared/helpers/customToast";
+import { useExceptionHandler } from "@/shared/hooks/useExceptionHandler";
 import { MutationOptions, useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-export const useLogout = (option: MutationOptions<void, AxiosError>) =>
-  useMutation<void, AxiosError>({
+export const useLogout = (option: MutationOptions<void, AxiosError>) => {
+  const exceptionHandler = useExceptionHandler();
+
+  const mutate = useMutation<void, AxiosError>({
     mutationFn: async () => {
       await axiosInstance.delete("/apis/auth");
     },
     onError: (err) => {
-      customToast("에러가 발생했습니다. 다시 시도해주세요.");
+      exceptionHandler(err, []);
     },
     ...option,
   });
+
+  return mutate;
+};
