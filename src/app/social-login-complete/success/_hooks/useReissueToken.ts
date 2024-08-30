619fb4972ff11757ab2d5c71c16027685c562da1
jochongs <joch2712@naver.com>
2024-08-30 17:57:42 +0900

    refactor: Apply exceptionHandler to social login complete page

--- src/app/social-login-complete/success/_hooks/useReissueToken.ts
+++ src/app/social-login-complete/success/_hooks/useReissueToken.ts
@@ -1,11 +1,14 @@
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import axiosInstance from "@/shared/helpers/axios";
+import { useExceptionHandler } from "@/shared/hooks/useExceptionHandler";
 
 export const useReissueToken = (
   props: UseMutationOptions<string, AxiosError, string>
-) =>
-  useMutation({
+) => {
+  const exceptionHandler = useExceptionHandler();
+
+  return useMutation({
     mutationFn: async (refreshToken) => {
       const { data } = await axiosInstance.post<string>(
         "/apis/auth/access-token",
@@ -17,5 +20,9 @@ export const useReissueToken = (
 
       return data;
     },
+    onError(err) {
+      exceptionHandler(err, [500, 502, 504]);
+    },
     ...props,
   });
+};
