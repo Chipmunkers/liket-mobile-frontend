f9c5afbee274043fb3e700b55f68e5f749400c82
jochongs <joch2712@naver.com>
2024-08-30 14:01:38 +0900

    feat: Add exception handler to soon open content section exception

--- src/app/_ui/SoonOpenContentSection.tsx
+++ src/app/_ui/SoonOpenContentSection.tsx
@@ -3,11 +3,20 @@
 import { SummaryContentEntity } from "@/shared/types/api/content/SummaryContentEntity";
 import { useGetSoonOpenContent } from "../_hooks/getSoonOpenContents";
 import ContentCardSection from "@/widgets/content/ContentSection";
+import { useEffect } from "react";
+import { useExceptionHandler } from "@/shared/hooks/useExceptionHandler";
 
 const SoonOpenContentSection = (props: {
   contentList: SummaryContentEntity[];
 }) => {
-  const { data } = useGetSoonOpenContent(props);
+  const exceptionHandler = useExceptionHandler();
+  const { data, error } = useGetSoonOpenContent(props);
+
+  useEffect(() => {
+    if (!error) return;
+
+    exceptionHandler(error, [418, 429, 500, 502, 504]);
+  }, [error]);
 
   return (
     <ContentCardSection contentList={data.contentList}>
