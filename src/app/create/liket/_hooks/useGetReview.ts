3e0050a501d7742b67cfe77eba825ef6dbea2b07
Younghoo Kim <sensitive.hu@gmail.com>
2024-09-04 00:28:08 +0900

    chore: 상세 리뷰 조회에 대한 타입 설정

--- src/app/create/liket/_hooks/useGetReview.ts
+++ src/app/create/liket/_hooks/useGetReview.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/shared/helpers/axios";
-import { ContentEntity } from "@/shared/types/api/content/ContentEntity";
+import { ReviewEntity } from "@/shared/types/api/review/ReviewEntity";
 
 export const useGetReview = (idx: number) =>
   useQuery({
     queryKey: [`review-${idx}`],
     queryFn: async () => {
-      const { data } = await axiosInstance.get<ContentEntity>(
+      const { data } = await axiosInstance.get<ReviewEntity>(
         `/apis/review/${idx}`
       );
 
