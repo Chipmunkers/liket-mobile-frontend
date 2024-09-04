886a222d3f2c20745d517cbbd63bc78705e2bc39
jochongs <joch2712@naver.com>
2024-09-04 18:17:53 +0900

    fix: Fix carousel scroll tolerance

--- src/app/_ui/MainBannerCarousel/index.tsx
+++ src/app/_ui/MainBannerCarousel/index.tsx
@@ -14,7 +14,7 @@ const MainBannerCarousel = ({ srcList }: Props) => {
       showStatus={false}
       showThumbs={false}
       emulateTouch={true}
-      swipeScrollTolerance={100}
+      swipeScrollTolerance={70}
       preventMovementUntilSwipeScrollTolerance={true}
     >
       {srcList.map((imgPath, index) => {
