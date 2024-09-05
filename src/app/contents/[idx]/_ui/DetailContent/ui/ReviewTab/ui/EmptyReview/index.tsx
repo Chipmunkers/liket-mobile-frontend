73594a03adc4ad175f8d4aff9e47f927651bffa1
jochongs <joch2712@naver.com>
2024-09-05 14:10:12 +0900

    feat: Apply safe area bottom to content review page

--- src/app/contents/[idx]/_ui/DetailContent/ui/ReviewTab/ui/EmptyReview/index.tsx
+++ src/app/contents/[idx]/_ui/DetailContent/ui/ReviewTab/ui/EmptyReview/index.tsx
@@ -6,12 +6,17 @@ import { useRouter } from "next/navigation";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
 import { Props } from "./types";
+import { useGetSafeArea } from "@/shared/hooks/useGetSafeArea";
 
 const EmptyReview = (props: Props) => {
   const router = useRouter();
+  const { safeArea } = useGetSafeArea();
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div
+      className="flex flex-col justify-center items-center"
+      style={{ paddingBottom: safeArea.bottom + "px" }}
+    >
       <div className="flex flex-col w-[200px] items-center mt-[24px]">
         <p className="text-grey-04 text-body3">작성된 리뷰가 없습니다.</p>
         <p className="text-grey-04 text-body3">첫번째로 리뷰를 남겨보세요!</p>
