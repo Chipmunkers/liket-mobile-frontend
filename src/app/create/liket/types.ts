d6f6bd8d94f4924ad26428fbe1701fcce4b69246
Younghoo Kim <sensitive.hu@gmail.com>
2024-09-10 00:04:51 +0900

    chore: liket 관련 타입 추가

--- src/app/create/liket/types.ts
+++ src/app/create/liket/types.ts
@@ -13,3 +13,37 @@ export type CardSizeType = keyof typeof CARD_SIZE;
 export type ColorTokensType = (typeof COLOR_TOKENS)[number];
 
 export type StrictShapeConfig = RequireProperty<ShapeConfig, "id">;
+
+export interface TextShape {
+  type: "text";
+  id: number; // front에서 임의로 생성하고있음.
+  fill: string; // 컬러값
+  text: string;
+  x: string;
+  y: string;
+}
+
+export interface ImageShape {
+  type: "image";
+  id: number;
+  imageSrc: string;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}
+
+export interface CardImageInformation {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  angle: number;
+}
+
+export interface Payload {
+  shapes: (TextShape | ImageShape)[];
+  cardImageSrc: string; // Blob Data URL
+  cardSize: "SMALL" | "MEDIUM" | "LARGE";
+  cardImageInformation: CardImageInformation;
+}
