f83a8006d9d8744271001bf25bb3708f2206b7ee
jochongs <joch2712@naver.com>
2024-09-10 14:03:35 +0900

    refactor: Refactoring Content detail carousel

--- src/widgets/content/ContentImgCarousel/index.tsx
+++ src/widgets/content/ContentImgCarousel/index.tsx
@@ -1,36 +1,11 @@
 "use client";
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
-import DefaultImg from "@/shared/ui/DefaultImg";
 import { Props } from "./types";
+import ImgCarousel from "@/shared/ui/ImgCarousel";
 
 const ContentImgCarousel = ({ list }: Props) => {
   return (
-    <Carousel
-      infiniteLoop
-      showArrows={false}
-      showStatus={false}
-      showThumbs={false}
-      emulateTouch={true}
-      swipeScrollTolerance={10}
-      preventMovementUntilSwipeScrollTolerance={true}
-    >
-      {list.map((imgPath, index) => {
-        return (
-          <div
-            key={imgPath + index}
-            className="relative"
-            style={{
-              width: "100%",
-              aspectRatio: "1 / 1",
-            }}
-          >
-            <DefaultImg src={imgPath} />
-          </div>
-        );
-      })}
-    </Carousel>
+    <ImgCarousel imgList={list} onClickImg={() => {}} imgAlt="컨텐츠 이미지" />
   );
 };
 
