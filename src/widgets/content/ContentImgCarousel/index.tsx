4745a1f645aea1b4b6069b468393e2c36dd829da
jochongs <joch2712@naver.com>
2024-09-10 19:19:15 +0900

    fix: Fix content img detail bug

--- src/widgets/content/ContentImgCarousel/index.tsx
+++ src/widgets/content/ContentImgCarousel/index.tsx
@@ -5,14 +5,23 @@ import { Props } from "./types";
 import ImgCarousel from "@/shared/ui/ImgCarousel";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
+import { useIsWebView } from "@/shared/hooks/useIsWebview";
 
 const ContentImgCarousel = ({ list }: Props) => {
   const router = useRouter();
+  const isWebview = useIsWebView();
 
   return (
     <ImgCarousel
       imgList={list}
       onClickImg={(path, i) => {
+        if (!isWebview) {
+          router.push(
+            window.location.pathname + "?" + `content-img-index=${i}`
+          );
+          return;
+        }
+
         stackRouterPush(router, {
           path:
             `/img-detail?select=${i}&` +
