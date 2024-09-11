ef3f79492fa8d6a431b5ca48e40a42d0cdbfb620
Younghoo Kim <sensitive.hu@gmail.com>
2024-09-12 00:27:41 +0900

    chore: 불필요한 타입 제거

--- src/app/create/liket/_hooks/useGetLiket.ts
+++ src/app/create/liket/_hooks/useGetLiket.ts
@@ -9,35 +9,3 @@ export const useGetLiket = ({ idx = 1 }) =>
       });
     },
   });
-
-interface TextShape {
-  type: "text";
-  id: number; // front에서 임의로 생성하고있음.
-  fill: string; // 컬러값
-  text: string;
-  x: string;
-  y: string;
-}
-
-interface ImageShape {
-  type: "image";
-  id: number;
-  imageSrc: string;
-  width: number;
-  height: number;
-  x: number;
-  y: number;
-}
-
-interface Payload {
-  shapes: (TextShape | ImageShape)[];
-  cardImageSrc: string; // Blob Data URL
-  cardSize: "SMALL" | "MEDIUM" | "LARGE";
-  cardImageInformation: {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    angle: number;
-  };
-}
