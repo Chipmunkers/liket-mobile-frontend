99968d8640da2bbce0df9deba95c7c382ab9cdcd
jochongs <joch2712@naver.com>
2024-09-19 23:08:29 +0900

    refactor: Refactoring bottom tab click event logic

--- src/widgets/common/BottomTab/hooks/useMessageWebview.ts
+++ src/widgets/common/BottomTab/hooks/useMessageWebview.ts
@@ -1,16 +1,36 @@
 import { WEBVIEW_EVENT_TYPE } from "@/shared/consts/webview/event";
 import { setAppNavBack } from "@/shared/helpers/setAppNavState";
 import { SetState } from "@/shared/types/react";
+import useGetClickEvent from "@/widgets/common/BottomTab/hooks/useGetClickEvent";
 import { useEffect } from "react";
 
 const useMessageWebview = (isOpen: boolean, setIsOpen: SetState<boolean>) => {
+  const {
+    mainButtonClickEvent,
+    mapButtonClickEvent,
+    createButtonClickEvent,
+    mypageButtonClickEvent,
+  } = useGetClickEvent(setIsOpen);
+
   const messageEvent = (e: MessageEvent) => {
     if (typeof e.data !== "string") return;
 
     const data: { type: string; click: string } = JSON.parse(e.data);
     if (data.type === WEBVIEW_EVENT_TYPE.CLICK) {
+      if (data.click === "nav-main-button") {
+        return mainButtonClickEvent();
+      }
+
+      if (data.click === "nav-map-button") {
+        return mapButtonClickEvent();
+      }
+
       if (data.click === "nav-create-button") {
-        setIsOpen(true);
+        return createButtonClickEvent();
+      }
+
+      if (data.click === "nav-mypage-button") {
+        return mypageButtonClickEvent();
       }
     }
   };
