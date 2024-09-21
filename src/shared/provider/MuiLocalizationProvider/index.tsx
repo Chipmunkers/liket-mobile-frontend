7aafd404bb5ffc12a2ff268d14b69caf387d5176
livi-younghoo <sensitive.hu@gmail.com>
2024-09-21 22:26:19 +0900

    feat: 캘린더 로케일 한국어로 설정

--- src/shared/provider/MuiLocalizationProvider/index.tsx
+++ src/shared/provider/MuiLocalizationProvider/index.tsx
@@ -3,12 +3,13 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { PropsWithChildren } from "react";
+import "dayjs/locale/ko";
 
 type Props = PropsWithChildren;
 
 const MuiLocalizationProvider = ({ children }: Props) => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
       {children}
     </LocalizationProvider>
   );
