0a7e36c476eb6cebf085297478eefde5498bdd8b
livi-younghoo <sensitive.hu@gmail.com>
2024-09-21 21:54:18 +0900

    chore: text-area util class 생성 및 적용

--- src/app/create/inquiry/page.tsx
+++ src/app/create/inquiry/page.tsx
@@ -187,7 +187,7 @@ export default function Page() {
               <TextareaAutosize
                 maxLength={200}
                 placeholder="내용을 입력해주세요."
-                className="w-[100%] min-h-[132px] px-[8px] py-[16px] mt-[8px] placeholder:text-body3 placeholder:text-grey-02 border-y-[1px] focus:outline-none focus:ring-0 rounded-none"
+                className="text-area"
                 {...register("description")}
               />
             </div>
