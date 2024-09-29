fa3a1d1e7a303a8b27d10d32bd6c0065695baa66
jochongs <joch2712@naver.com>
2024-09-29 17:32:09 +0900

    feat: Implement content map page getting content feature

---
+++ src/app/content-map/[idx]/_ui/GoogleMap/hooks/useGetCultureContentByIdx.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/shared/helpers/axios";
+import { ContentEntity } from "@/shared/types/api/content/ContentEntity";
+
+export const useGetCultureContentByIdx = (
+  idx: number,
+  initialData: ContentEntity
+) => {
+  const query = useQuery({
+    queryKey: [`culture-content-${idx}`],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get<ContentEntity>(
+        `/apis/culture-content/${idx}`
+      );
+
+      return data;
+    },
+    initialData,
+    staleTime: 0,
+    refetchOnWindowFocus: false,
+  });
+
+  return query;
+};
---
+++ src/app/content-map/[idx]/_ui/GoogleMap/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Props } from "./tyeps";
+import { stackRouterPush } from "@/shared/helpers/stackRouter";
+import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
+
+const GoogleMap = ({ content }: Props) => {
+  const router = useRouter();
+
+  if (!content) {
+    stackRouterPush(router, {
+      path: "/error/wrong-access",
+      screen: WEBVIEW_SCREEN.ERROR,
+      isStack: false,
+    });
+
+    return;
+  }
+
+  return <div></div>;
+};
+
+export default GoogleMap;
