bfc129ccd6e3ee697a2d9f35b733c92e13971611
jochongs <joch2712@naver.com>
2024-09-30 16:11:21 +0900

    feat: Implement content title in map content detail page

--- src/app/content-map/[idx]/_ui/DetailGoogleMap/index.tsx
+++ src/app/content-map/[idx]/_ui/DetailGoogleMap/index.tsx
@@ -4,7 +4,13 @@ import { useRouter } from "next/navigation";
 import { Props } from "./tyeps";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
-import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  MarkerF,
+  OverlayView,
+  OverlayViewF,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 import { mapStyle } from "@/app/map/_ui/GoogleMap/style/mapStyle";
 
 const DetailGoogleMap = ({ content }: Props) => {
@@ -58,6 +64,37 @@ const DetailGoogleMap = ({ content }: Props) => {
               url: `https://liket.s3.ap-northeast-2.amazonaws.com/map-marker/click_marker_${content.genre.idx}_icon.svg`,
             }}
           />
+          <OverlayViewF
+            position={{
+              lat: content.location.positionY,
+              lng: content.location.positionX,
+            }}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div
+              className="
+                translate-x-[-50%] 
+                translate-y-[4px] 
+                bg-grey-black/60 
+                bg-opacity-60
+                rounded-[8px] 
+                py-[1px] 
+                text-white 
+                px-[8px] 
+                max-w-[100px] 
+                text-center 
+                text-caption
+                stroke-black
+                "
+              style={{
+                textShadow:
+                  "-1px -1px 0 #222, 1px -1px 0 #222, -1px 1px 0 #222, 1px 1px 0 #222",
+                backgroundColor: "rgba(0, 0, 0, 0.6)",
+              }}
+            >
+              {content.title}
+            </div>
+          </OverlayViewF>
         </GoogleMap>
       )}
     </div>
