67bc0d6bc24e6ff53151ef2be007b4da05e890d0
jochongs <joch2712@naver.com>
2024-09-30 15:57:15 +0900

    feat: Implement map content detail page map component

--- src/app/content-map/[idx]/_ui/DetailGoogleMap/index.tsx
+++ src/app/content-map/[idx]/_ui/DetailGoogleMap/index.tsx
@@ -4,9 +4,16 @@ import { useRouter } from "next/navigation";
 import { Props } from "./tyeps";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { mapStyle } from "@/app/map/_ui/GoogleMap/style/mapStyle";
 
 const DetailGoogleMap = ({ content }: Props) => {
   const router = useRouter();
+
+  // TODO: shared로 분리 필요
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || "",
+  });
 
   if (!content) {
     stackRouterPush(router, {
@@ -18,7 +25,43 @@ const DetailGoogleMap = ({ content }: Props) => {
     return;
   }
 
-  return <div></div>;
+  return (
+    <div className="flex flex-1 bg-grey-02">
+      {isLoaded && (
+        <GoogleMap
+          mapContainerClassName="flex-1"
+          zoom={14}
+          key={`${isLoaded}`}
+          options={{
+            disableDefaultUI: true,
+            minZoom: 10,
+            // TODO: shared 로 분리 필요
+            styles: mapStyle,
+            keyboardShortcuts: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => {
+            map.setCenter({
+              lat: content.location.positionY,
+              lng: content.location.positionX,
+            });
+          }}
+        >
+          <MarkerF
+            key={content.idx}
+            position={{
+              lat: content.location.positionY,
+              lng: content.location.positionX,
+            }}
+            title={content.title}
+            icon={{
+              url: `https://liket.s3.ap-northeast-2.amazonaws.com/map-marker/click_marker_${content.genre.idx}_icon.svg`,
+            }}
+          />
+        </GoogleMap>
+      )}
+    </div>
+  );
 };
 
 export default DetailGoogleMap;
