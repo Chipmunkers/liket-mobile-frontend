3b3fa141f710f670368cc29a4149af0babb65580
Younghoo Kim <sensitive.hu@gmail.com>
2024-11-05 23:46:36 +0900

    feat: 약관 페이지에 마크다운 적용 (#119)
    * chore: 마크다운 관련 패키지 설치
    
    * chore: prose 사용을 위한 tailwind 셋업
    
    * feat: 정책 관련 페이지에 markdown 스타일 적용

--- src/app/terms/[idx]/_ui/DetailTerms/index.tsx
+++ src/app/terms/[idx]/_ui/DetailTerms/index.tsx
@@ -2,18 +2,30 @@
 
 import { useGetSafeArea } from "@/shared/hooks/useGetSafeArea";
 import { Props } from "./types";
+import Markdown from "react-markdown";
+import styled from "@emotion/styled";
 
 const DetailTerms = ({ contents }: Props) => {
   const { safeArea } = useGetSafeArea();
 
   return (
     <main
-      className="px-[24px] pt-[24px] mb-[24px]"
-      style={{ paddingBottom: safeArea.bottom + "px" }}
+      id="markdown"
+      className="px-[24px] pt-[24px] mb-[24px] prose"
+      style={{
+        paddingBottom: safeArea.bottom + "px",
+      }}
     >
-      {contents}
+      <StyledMarkdown className={"prose"}>{contents}</StyledMarkdown>
     </main>
   );
 };
 
 export default DetailTerms;
+
+const StyledMarkdown = styled(Markdown)(() => ({
+  li: {
+    listStyle: "inherit",
+    listStyleType: "inherit",
+  },
+}));
