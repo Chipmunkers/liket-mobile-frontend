00d2d766b30b64658eb0c95e55b68956d609997c
wherehows <sensitive.hu@gmail.com>
2024-11-09 00:08:50 +0900

    fix: 파일 이름으로 인한 eslint 에러로 빌드가 안되는 이슈

--- src/app/terms/[idx]/_hooks/getTosByIdx.ts
+++ src/app/terms/[idx]/_hooks/getTosByIdx.ts
@@ -1,5 +1,5 @@
 import customFetch from "@/shared/helpers/fetch";
 import { TosEntity } from "@/shared/types/api/terms-of-service/TosEntity";
 
-export const useGetTosByIdx = async (idx: number): Promise<TosEntity> =>
+export const getTosByIdx = async (idx: number): Promise<TosEntity> =>
   (await customFetch(`/tos/${idx}`)).json();
