8f50be8c5faf4fde4cf82e31e69279e4e5690a9e
wherehows <sensitive.hu@gmail.com>
2024-11-15 00:44:20 +0900

    feat: TermsItem 컴포넌트 수정

--- src/app/terms-agreement/_ui/TermsItem/index.tsx
+++ src/app/terms-agreement/_ui/TermsItem/index.tsx
@@ -1,14 +1,25 @@
+import RightArrow from "@/icons/right-arrow.svg";
 import CheckBox from "@/shared/ui/CheckBox";
 import { Props } from "./types";
 
-const TermsItem = ({ tos, isChecked, onChange }: Props) => {
+const TermsItem = ({
+  title,
+  isChecked,
+  isEssential,
+  onClickArrow,
+  onChange,
+}: Props) => {
   return (
-    <div className="my-[8px]">
+    <div className="flex items-center justify-between my-[8px]">
       <CheckBox
         isChecked={isChecked}
-        label={`[필수] ${tos.title}`}
+        label={isEssential ? `[필수] ${title}` : `${title}`}
         onChange={onChange}
+        marginBetweenTextAndCheckbox={"8px"}
       />
+      <button onClick={onClickArrow}>
+        <RightArrow />
+      </button>
     </div>
   );
 };
