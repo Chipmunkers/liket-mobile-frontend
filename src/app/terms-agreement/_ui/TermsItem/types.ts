7ac24a16021f1659cb98f366fbcbf767edc32015
Younghoo Kim <sensitive.hu@gmail.com>
2024-11-15 00:46:14 +0900

    feat: 회원가입시 약관을 밟도록 코드 수정 (#123)
    * feat: 회원가입 페이지 개발
    
    * feat: terms-agreement 페이지 개발
    
    * feat: TermsItem 컴포넌트 수정
    
    * feat: Checkbox 컴포넌트 수정
    
    * chore: 서비스훅 관련 수정
    
    * feat: PolicyForm 컴포넌트 추가
    
    * feat: TermsItem 컴포넌트 관련 타입 수정

--- src/app/terms-agreement/_ui/TermsItem/types.ts
+++ src/app/terms-agreement/_ui/TermsItem/types.ts
@@ -1,13 +1,14 @@
 import { SummaryTosEntity } from "@/shared/types/api/terms-of-service/SummaryTosEntity";
 
 export type Props = {
-  tos: SummaryTosEntity;
-
   /**
    * 체크 되어있는지 여부
    *
    * @default false
    */
   isChecked: boolean;
+  title: string;
+  isEssential: boolean;
+  onClickArrow: () => void;
   onChange: () => void;
 };
