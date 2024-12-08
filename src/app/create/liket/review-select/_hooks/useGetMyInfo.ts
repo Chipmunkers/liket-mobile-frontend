b377d4de7282450962cb328eb9d6cc5fed16fc93
Younghoo Kim <sensitive.hu@gmail.com>
2024-12-08 13:49:18 +0900

    fix: 빌드 에러 수정 (#127)
    * chore: idx 타입을 string으로 변경
    
    * chore: useGetMyInfo 훅을 review-select를 위한 훅과 공용 훅으로 분리 개발
    
    * chore: useGetMyInfo 훅이 공용 훅과 review-select 훅이 구분되어 적용되도록 수정

---
+++ src/app/create/liket/review-select/_hooks/useGetMyInfo.ts
@@ -0,0 +1,50 @@
+import axiosInstance from "@/shared/helpers/axios";
+import { MutationOptions, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { MyInfoEntity } from "@/shared/types/api/user/MyInfoEntity";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { stackRouterPush } from "@/shared/helpers/stackRouter";
+import { WEBVIEW_SCREEN } from "@/shared/consts/webview/screen";
+import { useExceptionHandler } from "@/shared/hooks/useExceptionHandler";
+
+export const useGetMyInfo = (
+  options: MutationOptions<MyInfoEntity, AxiosError>
+) => {
+  const router = useRouter();
+  const exceptionHandler = useExceptionHandler();
+
+  const query = useQuery<MyInfoEntity, AxiosError>({
+    queryKey: [],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get<MyInfoEntity>("/apis/user/my");
+      return data;
+    },
+    staleTime: 0,
+    ...options,
+  });
+
+  useEffect(() => {
+    if (!query.error) return;
+
+    exceptionHandler(query.error, [
+      {
+        statusCode: 401,
+        handler() {
+          stackRouterPush(router, {
+            path: "/login",
+            screen: WEBVIEW_SCREEN.LOGIN,
+            isStack: false,
+          });
+        },
+      },
+      418,
+      429,
+      500,
+      502,
+      504,
+    ]);
+  }, [query.error]);
+
+  return query;
+};
