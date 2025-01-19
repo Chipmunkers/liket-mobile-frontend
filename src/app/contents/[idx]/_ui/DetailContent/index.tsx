f9e8f5b8e3a406c21e2b28fced7c085dab6f7302
wherehows <sensitive.hu@gmail.com>
2025-01-19 11:33:20 +0900

    feat: 푸더 디자인 수정

--- src/app/contents/[idx]/_ui/DetailContent/index.tsx
+++ src/app/contents/[idx]/_ui/DetailContent/index.tsx
@@ -174,15 +174,16 @@ const DetailContent = (props: Props) => {
         )}
       </main>
       <Divider width="100%" height="8px" />
-      <footer className="text-sm text-center p-4 ">
+      <footer className="text-sm text-left p-[24px] text-body5 text-grey-04">
         <div>
-          본 사이트는 한국문화정보원 KOPIS API와 한국관광공사 TourAPI의
+          * 본 사이트는 한국문화정보원 KOPIS API와 한국관광공사 TourAPI의
           공공데이터를 활용하고 있습니다.
         </div>
-        <div className="flex justify-center" style={{}}>
+        <div>
           <div>
             제공:{" "}
             <Link
+              className="underline"
               rel="noopener noreferrer"
               target="_blank"
               href="https://www.kopis.or.kr/por/cs/openapi/openApiInfo.do?menuId=MNU_00074"
@@ -191,6 +192,7 @@ const DetailContent = (props: Props) => {
             </Link>{" "}
             |{" "}
             <Link
+              className="underline"
               rel="noopener noreferrer"
               target="_blank"
               href="https://api.visitkorea.or.kr/"
