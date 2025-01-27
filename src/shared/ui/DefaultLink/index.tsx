487f0284442573a9ff31ee0f60a8cd73c118f336
jochongs <joch2712@naver.com>
2025-01-27 13:16:53 +0900

    chore: Add code comments on DefaultLink

--- src/shared/ui/DefaultLink/index.tsx
+++ src/shared/ui/DefaultLink/index.tsx
@@ -7,6 +7,12 @@ import { isExternalLink } from "@/shared/ui/DefaultLink/utils/isExternalLink";
 import { stackRouterPush } from "@/shared/helpers/stackRouter";
 import { classNames } from "@/shared/helpers/classNames";
 
+/**
+ * 링크 태그 대신 사용하는 Link 태그
+ *
+ * routerOption을 넣을 경우 앱뷰에서 스크린이 전환됩니다.
+ * 넣지 않은 경우, 스크린이 전환되지 않으며 외부 링크의 경우 새로운 창이 열립니다.
+ */
 export const DefaultLink = ({
   href,
   routerOption,
