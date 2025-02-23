e3686c649e5d8f1dfae9913b24f0adb7b5992b34
wherehows <sensitive.hu@gmail.com>
2025-02-23 13:50:18 +0900

    refactor: Locaiton Drawer 리펙토링

--- src/app/map/_ui/LocationDrawer/types.ts
+++ src/app/map/_ui/LocationDrawer/types.ts
@@ -1,17 +1,9 @@
 import { SelectLocation } from "../../_types/types";
-import { Sido } from "@/shared/consts/region/sido";
-import { Sigungu } from "@/shared/consts/region/sigungu";
 import { SetState } from "@/shared/types/react";
 
 export type Props = {
   isOpen: boolean;
-
-  selectSido: Sido;
-  setSelectSido: SetState<Sido>;
-
-  selectSigungu: Sigungu | null;
-  setSelectSigungu: SetState<Sigungu | null>;
-
   selectLocation: SelectLocation;
   setSelectLocation: SetState<SelectLocation>;
+  setLatLng: SetState<{ lat: number; lng: number }>;
 };
