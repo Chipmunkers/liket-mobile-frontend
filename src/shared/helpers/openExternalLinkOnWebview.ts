49b89e0f455fb57fe5bda529c2cb4a977621d867
daangnkim <yang.daangn@gmail.com>
2025-03-23 21:27:32 +0900

    chore: messageToRN 적용

--- src/shared/helpers/openExternalLinkOnWebview.ts
+++ src/shared/helpers/openExternalLinkOnWebview.ts
@@ -1,11 +1,11 @@
 import { WEBVIEW_EVENT_TYPE } from "@/shared/consts/webview/event";
+import { messageToRN } from "./messageToRN";
 
 export const openExternalLinkOnWebview = (href: string) => {
   if (!window.ReactNativeWebView) return;
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      type: WEBVIEW_EVENT_TYPE.OPEN_EXTERNAL_LINK,
-      href,
-    })
-  );
+
+  messageToRN({
+    type: WEBVIEW_EVENT_TYPE.OPEN_EXTERNAL_LINK,
+    href,
+  });
 };
