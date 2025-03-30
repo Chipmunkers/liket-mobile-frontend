c6c4731d2b74ea0b70fed6388459faae8fb3d69f
daangnkim <yang.daangn@gmail.com>
2025-03-30 21:33:01 +0900

    feat(DetailContent): 카테고리 탭 변경시 animated tab으로 동작하도록 수정

--- src/shared/ui/CategoryTab/index.tsx
+++ src/shared/ui/CategoryTab/index.tsx
@@ -1,38 +1,65 @@
+import { useRef, useState } from "react";
 import { Props } from "./types";
 import { classNames } from "@/shared/helpers/classNames";
 
 const CategoryTab = ({
-  small = true,
   list,
   selectedTab,
   wrapperStyle,
   onClickTab,
   customTabNames,
 }: Props) => {
+  const listRef = useRef<(HTMLLIElement | null)[]>([]);
+  const [leftOffset, setLeftOffset] = useState(0);
+  const [width, setWidth] = useState(0);
+
   return (
     <ul
       style={wrapperStyle}
       className={classNames(
-        "flex w-[100%] px-[24px] border-b-[1px] border-grey-01",
-        small ? "h-[32px]" : "h-[40px]"
+        "relative flex w-[100%] px-[24px] border-b-[1px] border-grey-01 h-[40px]"
       )}
     >
-      {list.map((tab, i) => (
+      <span
+        className={classNames(
+          "h-full absolute border-b-[2px] border-skyblue-01 transition-all duration-300"
+        )}
+        style={{
+          left: leftOffset,
+          width: width,
+        }}
+      />
+      {list.map((tab, index) => (
         <li
+          ref={(ref) => {
+            listRef.current[index] = ref;
+
+            if (tab === selectedTab) {
+              setLeftOffset(listRef.current[index]?.offsetLeft ?? 0);
+              setWidth(listRef.current[index]?.clientWidth ?? 0);
+            }
+          }}
           key={tab}
           className={classNames(
             "grow h-[100%]",
             selectedTab === tab
-              ? "text-skyblue-01 border-b-[2px] border-skyblue-01 text-button3"
+              ? "text-skyblue-01 text-button3"
               : "text-button4 text-grey-03 pb-[2px]",
-            small ? "text-button4" : "text-button3"
+            "text-button3"
           )}
         >
           <button
-            className="w-[100%] h-[100%] center"
-            onClick={() => onClickTab(tab)}
+            className="size-full center"
+            onClick={() => {
+              onClickTab(tab);
+
+              if (tab === selectedTab) {
+                setLeftOffset(listRef.current[index]?.offsetLeft ?? 0);
+                setWidth(listRef.current[index]?.clientWidth ?? 0);
+              }
+            }}
           >
-            {customTabNames?.[i] || tab}
+            {customTabNames?.[index] || tab}
           </button>
         </li>
       ))}
